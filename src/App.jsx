d688cac23539a797da079b7a738be70b5dc79010
HseinKt <hseinkteish1998@gmail.com>
2025-04-22 16:00:59 +0300

    Added Footer style

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,24 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import BugetTracker from './pages/BugetTracker'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
+    <div className="App-container">
       <Router>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/tracker" element={<BugetTracker/>} />
-          <Route path="*" element={<NotFound/>} />
-        </Routes>
+        <main className="Main-content">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/tracker" element={<BugetTracker/>} />
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+        </main>
       </Router>
-    </>
+
+      <Footer/>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import '../CSS/Home.css';
 
 const Home = () => {
     return ( 
         <>
-            Home
-            <h1>Welcome to the Home Page</h1>
-            <Header/>
-            <Footer/>
+            <div>
+                <h1>Welcome to the Expense Tracker</h1>
+                <p>Track your expenses and manage your budget effectively.</p>
+                <p>Click the button below to get started!</p>
+                <Link className="home-tracker" to="/tracker"> 
+                    <button className="btn">Track your expenses</button>
+                </Link>
+            </div>            
         </>
         
      );
