0aa11061ba65b2f76d58ebf61101a3bf65e597e8
HseinKt <hseinkteish1998@gmail.com>
2025-04-22 17:12:20 +0300

    Added Home page and style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,21 @@ import '../CSS/Home.css';
 const Home = () => {
     return ( 
         <>
-            <div>
-                <h1>Welcome to the Expense Tracker</h1>
-                <p>Track your expenses and manage your budget effectively.</p>
-                <p>Click the button below to get started!</p>
-                <Link className="home-tracker" to="/tracker"> 
-                    <button className="btn">Track your expenses</button>
-                </Link>
+            <div className="home-container">
+                <div className="home-header">
+                    <h1>Welcome to the Expense Tracker</h1>
+                    <Link className="home-tracker" to="/tracker"> 
+                        <button className="btn">Track your expenses</button>
+                    </Link>
+                </div>
+
+                <div className="home-content">
+                    <h3>💡 The Power of Tracking Our Expenses</h3>
+                    <p className="home-text">
+                        Tracking our expenses is a powerful habit that brings clarity and control over our financial life. It helps us understand where our money goes, identify unnecessary spending, and make smarter decisions with our income. Whether it's saving for a goal, avoiding debt, or simply building better financial awareness, a good expense tracker transforms spending into insight. With this app, users can visualize their habits, set limits, and take actionable steps toward financial freedom — one transaction at a time.
+                    </p>
+                </div>
+                
             </div>            
         </>
         
